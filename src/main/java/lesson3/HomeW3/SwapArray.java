fa51e70d79ea5ff675b7a58aa64bb23a6536d5f1
Dima <ninehands@yandex.ru>
2022-05-30 14:49:26 +0300

    less4

---
+++ src/main/java/lesson3/HomeW3/SwapArray.java
@@ -0,0 +1,15 @@
+package lesson3.HomeW3;
+
+public class SwapArray {
+    public static void main(String[] args) {
+       /* SwapArr(new int[][]{new int[]{1, 3, 6, 7,}}, 2,2 );
+    }
+    public static void SwapArr(Object[] Array, Object i, Object j){
+        System.out.println(Array[]);
+        Object Swap=Array[(int) i];
+        Array[(int) i] =Array[(int) j];
+        Array[(int) j] = Swap;
+        System.out.println(Array.toString());
+    }*/
+    }
+}
